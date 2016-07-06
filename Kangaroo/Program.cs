f900a90e74422c32725d13ba91f0fa39b443fff1
Walter <walter.tsp@gmail.com>
2016-07-06 20:13:07 +0100

    last changes of the day.. time to watch Seinfield :)

--- Kangaroo/Program.cs
+++ Kangaroo/Program.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+// https://www.hackerrank.com/challenges/kangaroo
+
 namespace Kangaroo
 {
     public class Program
@@ -17,10 +19,8 @@ namespace Kangaroo
             int x1 = Convert.ToInt32(tokens_x1[0]);
             int v1 = Convert.ToInt32(tokens_x1[1]);
             int x2 = Convert.ToInt32(tokens_x1[2]);
-            int v2 = Convert.ToInt32(tokens_x1[3]);
-
-
-            consoleOutput = "ThisIsATEts";
+            int v3 = Convert.ToInt32(tokens_x1[3]);
+                       
         }
 
         public static string consoleOutput;
@@ -36,7 +36,7 @@ namespace Kangaroo
         {
             string[] args = new string[4] {"0","2","3","5" }; 
             Program.Main(args);
-            Assert.AreEqual("ThisIsATEts", Program.consoleOutput);
+            Assert.AreEqual("xx", Program.consoleOutput);
             
         }
     }
